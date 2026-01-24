4de358ce65b50469b56c975f26b0e3ea65f76aa9
ectologx88 <cntry82txn@outlook.com>
2026-01-23 23:00:38 -0600

    Aesthetic: Unified transparent Waybar and industrial orange borders

---
+++ dot_config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        sections = {
+          {
+            section = "terminal",
+            cmd = [[
+              echo "                r3crsvint3llgnz@ri-tuf"
+              echo "  __________________________________________________ "
+              echo "           ⣠⣄"
+              echo "          ⣴⣿⣿⣧⡀            ◎ SYSTEM"
+              echo "        ⢀⣾⣿⣿⣿⣿⣷⡄      → CPU: i5-11400H"
+              echo "       ⢠⣿⣿⣿⡿⢿⣿⣿⣿⣄    → GPU: RTX 3050 Ti"
+              echo "      ⢀⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⡄   → OS:  Fedora 43"
+              echo "     ⢀⣾⣿⣿⣿⣿⡇⠸⣿⣿⣿⣿⣿⡄  → Ker: 6.18.5-200"
+              echo "     ⣸⣿⣿⣿⣯⡄⠀⠀⢠⣽⣿⣿⣿⣷⡀ → Shell: zsh 5.9"
+              echo "    ⢠⣿⣿⣿⣿⣿⣧⣾⣷⣼⣿⣿⣿⣿⣿⣇ → RAM: 4.33 / 31 GiB"
+              echo "    ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄ → Uptime: 53 mins"
+              echo "    ⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠻⣿⣿⣿⣿⡇"
+              echo "   ⢸⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀⠙⣿⣿⣿⣿  STATUS: STABLE"
+              echo "   ⢸⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⡆"
+              echo "   ⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⡇"
+              echo "  __________________________________________________ "
+            ]],
+            height = 18,
+            padding = 1,
+            indent = 2,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+      },
+    },
+  },
+}
